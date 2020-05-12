05e08402c0a972d6e0693952b244f6069929bcd5
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-12 00:54:39 -0700

    verified that name is unique when posting and updating

--- api/accounts/accountRouter.js
+++ api/accounts/accountRouter.js
@@ -1,35 +1,115 @@
-const express = require('express');
-const router = express.Router();
-const accountData = require('./accountHelperFunctions');
+const express = require('express')
+const router = express.Router()
+const accountData = require('../../data/dbConfig.js')
 
+// gets all the accounts from the database
 router.get('/', (req, res) => {
-    accountData.getAccounts()
-        .then(accounts => res.status(200).json(accounts))
-        .catch(() => res.status(500).json({ message: 'Accounts could not be retrieved' }))
+    accountData('accounts')
+        .then(accounts => {
+            res.status(200).json(accounts)
+        }) 
+        .catch (() => {
+            res.status(500).json({ message: 'Accounts could not be retrieved' })
+        })
 })
 
-router.post('/' , validateAccount, (req, res) => {
-    accountData.insertAccount(req.body)
-        .then(newAccount => res.status(200).json(newAccount))
-        .catch(err => res.status(500).json({ message: 'The account could not be inserted into the database.'}))
+// I already got the account when I validated the id and stored it in req.account
+router.get('/:id', validateAccountId, (req, res) => {
+    res.status(200).json(req.account)
 })
 
-router.put('/:id', validateAccount, validateAccountId, (req, res) => {
-    accountData.updateAccount(req.params.id, req.body)
-        .then(() => {
-            accountData.getById(req.params.id)
-            .then(account => res.status(200).json(account))
-            .catch(err => res.status(500).json({ message: 'The account could not be updated.'}))
-        });
+// inserts new account to the database
+router.post('/' , validateAccount, verifyUniqueName, (req, res) => {
+    accountData('accounts')
+        .insert(req.body)
+            .then(accountId => {
+                // this returns an array containing the id of the posted account. I've called it accountId
+                accountData('accounts').where({id: accountId[0]}).first()
+                    // now I want to return the inserted account
+                    .then(account => {
+                        res.status(200).json(account)
+                    })
+                    .catch(() => {
+                        res.status(500).json({ message: 'The account was inserted, but could not be returned'})
+                    })
+            })
+            .catch(() => {
+                res.status(500).json({ message: `The account with id of ${req.params.id} could not be inserted`})
+            })
 })
 
+// updates account already in database
+router.put('/:id', validateAccount, validateAccountId, verifyUniqueName, (req, res) => {
+    accountData('accounts').where({ id: req.params.id }).update(req.body)
+        .then(numberOfAccountsUpdated => {
+            if (numberOfAccountsUpdated === 1) {
+                // retrieve the account that was updated and return it to the client
+                accountData('accounts').where({ id: req.params.id }).first()
+                    .then(account => {
+                        res.status(200).json(account)
+                    })
+                    .catch(() => {
+                        res.status(500).json({ message: `The account with id of ${req.params.id} could not be retrieved after being updated`})
+                    })
+            } else {
+                res.status(500).json({ message: `The account with id of ${req.params.id} could not be updated`})
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: `The account with id of ${req.params.id} could not be updated`})
+        })
+})
+
+// deletes an account from the database
 router.delete('/:id', validateAccountId, (req, res) => {
-    accountData.removeAccount(req.params.id)
-        .then(() => res.status(200).json({ message: 'The account has been deleted' }))
-        .catch(err => res.status(500).json(err))
+    accountData('accounts').where({ id: req.params.id }).del()
+        .then(() => res.status(200).json({ message: `The account with id of ${req.params.id} has been deleted` }))
+        .catch(() => res.status(500).json({ message: `The account with id of ${req.params.id} could not be deleted`}))
 })
 
 // middleware
 
+function validateAccountId(req, res, next) {
+    // get account with the params id and assign it to req.account, otherwise return a 400 error
+    accountData('accounts')
+        .where({id: parseInt(req.params.id)}).first()
+            .then(account => {
+                    req.account = account
+                    next()
+            })
+            .catch(() => {
+                    res.status(400).json({ message: 'You requested with an invalid account id' })
+            })
+}
 
-module.exports = router;
+function verifyUniqueName(req, res, next) {
+    // get account with the name and if there is none then that means the incoming post is unique, otherwise return a 400 error
+    // this allows for name to be the same as the name of the account being updated, but no others
+    accountData('accounts')
+        .where({ name: req.body.name })
+            .then(accountWithSameNameAsRequestBody => {
+                if (accountWithSameNameAsRequestBody.length === 1 && parseInt(req.params.id) ===  accountWithSameNameAsRequestBody[0].id || accountWithSameNameAsRequestBody.length === 0) {
+                    next()
+                } else {
+                    res.status(400).json({ message: 'The new name must be unique to this account' })
+                }
+            })
+            .catch(() => {
+                    res.status(400).json({ message: 'You requested with an invalid account id' })
+            })
+}
+  
+function validateAccount(req, res, next) {
+    // send 400 error if req.body is missing, if req.body.name is missing, or if req.body.budget is missing
+    if (!req.body){
+        res.status(400).json({ message: 'missing account data' })
+    } else if (!req.body.name){
+        res.status(400).json({ message: 'missing required name field' })
+    } else if (!req.body.budget){
+        res.status(400).json({ message: 'missing required budget field' })
+    } else {
+        next()
+    }
+}
+
+module.exports = router
